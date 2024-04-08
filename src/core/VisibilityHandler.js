f462450595d2eb5abb97b69cc0ddc08285eca7a5
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:16 -0400

    improve

--- src/core/VisibilityHandler.js
+++ src/core/VisibilityHandler.js
@@ -4,7 +4,7 @@
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
-var Events = require('./events');
+const Events = require('./events');
 
 /**
  * The Visibility Handler is responsible for listening out for document level visibility change events.
@@ -20,66 +20,63 @@ var Events = require('./events');
  *
  * @param {Phaser.Game} game - The Game instance this Visibility Handler is working on.
  */
-var VisibilityHandler = function (game)
-{
-    var hiddenVar;
-    var eventEmitter = game.events;
+class VisibilityHandler {
+  constructor(game) {
+    this.hiddenVar;
+    this.eventEmitter = game.events;
 
-    if (document.hidden !== undefined)
-    {
-        hiddenVar = 'visibilitychange';
-    }
-    else
-    {
-        var vendors = [ 'webkit', 'moz', 'ms' ];
+    if (document.hidden !== undefined) {
+      this.hiddenVar = 'visibilitychange';
+    } else {
+      const vendors = ['webkit', 'moz', 'ms'];
 
-        vendors.forEach(function (prefix)
-        {
-            if (document[prefix + 'Hidden'] !== undefined)
-            {
-                document.hidden = function ()
-                {
-                    return document[prefix + 'Hidden'];
-                };
+      for (const prefix of vendors) {
+        if (document[prefix + 'Hidden'] !== undefined) {
+          document.hidden = function () {
+            return document[prefix + 'Hidden'];
+          };
 
-                hiddenVar = prefix + 'visibilitychange';
-            }
-
-        });
+          this.hiddenVar = prefix + 'visibilitychange';
+          break;
+        }
+      }
     }
 
-    var onChange = function (event)
-    {
-        if (document.hidden || event.type === 'pause')
-        {
-            eventEmitter.emit(Events.HIDDEN);
-        }
-        else
-        {
-            eventEmitter.emit(Events.VISIBLE);
-        }
+    const onChange = (event) => {
+      if (document.hidden || event.type === 'pause') {
+        this.eventEmitter.emit(Events.HIDDEN);
+      } else {
+        this.eventEmitter.emit(Events.VISIBLE);
+      }
     };
 
-    if (hiddenVar)
-    {
-        document.addEventListener(hiddenVar, onChange, false);
+    if (this.hiddenVar) {
+      document.addEventListener(this.hiddenVar, onChange, { passive: true, capture: false });
     }
 
-    window.onblur = function ()
-    {
-        eventEmitter.emit(Events.BLUR);
+    window.onblur = () => {
+      this.eventEmitter.emit(Events.BLUR);
     };
 
-    window.onfocus = function ()
-    {
-        eventEmitter.emit(Events.FOCUS);
+    window.onfocus = () => {
+      this.eventEmitter.emit(Events.FOCUS);
     };
 
     //  Automatically give the window focus unless config says otherwise
-    if (window.focus && game.config.autoFocus)
-    {
-        window.focus();
+    if (window.focus && game.config.autoFocus) {
+      window.focus();
     }
-};
+  }
+
+  dispose() {
+    if (this.hiddenVar) {
+      document.removeEventListener(this.hiddenVar, onChange);
+    }
+
+    window.onblur = null;
+    window.onfocus = null;
+  }
+}
 
 module.exports = VisibilityHandler;
+
