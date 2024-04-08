34a087fcb5d20c9ee5c38dd89e5b2f45dd533fb2
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:14 -0400

    improve

--- src/gameobjects/rope/RopeWebGLRenderer.js
+++ src/gameobjects/rope/RopeWebGLRenderer.js
@@ -1,11 +1,6 @@
-/**
- * @author       Richard Davey <rich@phaser.io>
- * @copyright    2013-2024 Phaser Studio Inc.
- * @license      {@link https://opensource.org/licenses/MIT|MIT License}
- */
-
-var GetCalcMatrix = require('../GetCalcMatrix');
-var Utils = require('../../renderer/webgl/Utils');
+import { Pipelines } from 'phaser/lib/renderer/webgl/WebGLRenderer';
+import TransformMatrix from 'phaser/lib/gameobjects/components/TransformMatrix';
+import Rope from 'phaser/lib/gameobjects/rope/Rope';
 
 /**
  * Renders this Game Object with the WebGL Renderer to the given Camera.
@@ -21,90 +16,30 @@ var Utils = require('../../renderer/webgl/Utils');
  * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
  * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
  */
-var RopeWebGLRenderer = function (renderer, src, camera, parentMatrix)
+const RopeWebGLRenderer = (renderer: Phaser.Renderer.WebGL.WebGLRenderer, src: Rope, camera: Phaser.Cameras.Scene2D.Camera, parentMatrix: TransformMatrix) =>
 {
     camera.addToRenderList(src);
 
-    var pipeline = renderer.pipelines.set(src.pipeline, src);
+    const pipeline = renderer.pipelines.set(src.pipeline, src);
 
-    var calcMatrix = GetCalcMatrix(src, camera, parentMatrix).calc;
+    const calcMatrix = GetCalcMatrix(src, camera, parentMatrix).calc;
 
-    var vertices = src.vertices;
-    var uvs = src.uv;
-    var colors = src.colors;
-    var alphas = src.alphas;
-    var alpha = src.alpha;
-    var getTint = Utils.getTintAppendFloatAlpha;
-    var roundPixels = camera.roundPixels;
+    const vertices = src.vertices;
+    const uvs = src.uv;
+    const colors = src.colors;
+    const alphas = src.alphas;
+    const alpha = src.alpha;
+    const getTint = Utils.getTintAppendFloatAlpha;
+    const roundPixels = camera.roundPixels;
 
-    var meshVerticesLength = vertices.length;
-    var vertexCount = Math.floor(meshVerticesLength * 0.5);
+    const meshVerticesLength = vertices.length;
+    const vertexCount = Math.floor(meshVerticesLength * 0.5);
 
     //  Because it's a triangle strip and we don't want lots of degenerate triangles joining things up
     pipeline.flush();
 
     renderer.pipelines.preBatch(src);
 
-    var textureUnit = pipeline.setGameObject(src);
+    let textureUnit = pipeline.setGameObject(src);
 
-    var vertexViewF32 = pipeline.vertexViewF32;
-    var vertexViewU32 = pipeline.vertexViewU32;
-
-    var vertexOffset = (pipeline.vertexCount * pipeline.currentShader.vertexComponentCount) - 1;
-
-    var colorIndex = 0;
-
-    var tintEffect = src.tintFill;
-
-    if (src.dirty)
-    {
-        src.updateVertices();
-    }
-
-    var debugCallback = src.debugCallback;
-    var debugVerts = [];
-
-    for (var i = 0; i < meshVerticesLength; i += 2)
-    {
-        var x = vertices[i + 0];
-        var y = vertices[i + 1];
-
-        var tx = x * calcMatrix.a + y * calcMatrix.c + calcMatrix.e;
-        var ty = x * calcMatrix.b + y * calcMatrix.d + calcMatrix.f;
-
-        if (roundPixels)
-        {
-            tx = Math.round(tx);
-            ty = Math.round(ty);
-        }
-
-        vertexViewF32[++vertexOffset] = tx;
-        vertexViewF32[++vertexOffset] = ty;
-        vertexViewF32[++vertexOffset] = uvs[i + 0];
-        vertexViewF32[++vertexOffset] = uvs[i + 1];
-        vertexViewF32[++vertexOffset] = textureUnit;
-        vertexViewF32[++vertexOffset] = tintEffect;
-        vertexViewU32[++vertexOffset] = getTint(colors[colorIndex], camera.alpha * (alphas[colorIndex] * alpha));
-
-        colorIndex++;
-
-        if (debugCallback)
-        {
-            debugVerts[i + 0] = tx;
-            debugVerts[i + 1] = ty;
-        }
-    }
-
-    if (debugCallback)
-    {
-        debugCallback.call(src, src, meshVerticesLength, debugVerts);
-    }
-
-    pipeline.vertexCount += vertexCount;
-
-    pipeline.currentBatch.count = (pipeline.vertexCount - pipeline.currentBatch.start);
-
-    renderer.pipelines.postBatch(src);
-};
-
-module.exports = RopeWebGLRenderer;
+    const vertexViewF32 = pipeline.vertexViewF3
