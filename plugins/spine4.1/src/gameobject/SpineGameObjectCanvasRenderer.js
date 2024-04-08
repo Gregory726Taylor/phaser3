c3c69d9a2687cd6921ace86bd650d7fe0bd74ce7
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:51 -0400

    improve

--- plugins/spine4.1/src/gameobject/SpineGameObjectCanvasRenderer.js
+++ plugins/spine4.1/src/gameobject/SpineGameObjectCanvasRenderer.js
@@ -4,9 +4,10 @@
  * @license      {@link https://github.com/photonstorm/phaser/blob/master/license.txt|MIT License}
  */
 
-var CounterClockwise = require('../../../../src/math/angle/CounterClockwise');
-var RadToDeg = require('../../../../src/math/RadToDeg');
-var Wrap = require('../../../../src/math/Wrap');
+const { CanvasRenderer } = require('phaser');
+const CounterClockwise = require('../../../../src/math/angle/CounterClockwise');
+const RadToDeg = require('../../../../src/math/RadToDeg');
+const Wrap = require('../../../../src/math/Wrap');
 
 /**
  * Renders this Game Object with the Canvas Renderer to the given Camera.
@@ -17,100 +18,88 @@ var Wrap = require('../../../../src/math/Wrap');
  * @since 3.19.0
  * @private
  *
- * @param {Phaser.Renderer.Canvas.CanvasRenderer} renderer - A reference to the current active Canvas renderer.
+ * @param {CanvasRenderer} renderer - A reference to the current active Canvas renderer.
  * @param {SpineGameObject} src - The Game Object being rendered in this call.
- * @param {Phaser.Cameras.Scene2D.Camera} camera - The Camera that is rendering the Game Object.
- * @param {Phaser.GameObjects.Components.TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
+ * @param {Camera} camera - The Camera that is rendering the Game Object.
+ * @param {TransformMatrix} parentMatrix - This transform matrix is defined if the game object is nested
  */
-var SpineGameObjectCanvasRenderer = function (renderer, src, camera, parentMatrix)
-{
-    var context = renderer.currentContext;
+const SpineGameObjectCanvasRenderer = function (renderer, src, camera, parentMatrix) {
+  const context = renderer.currentContext;
 
-    var plugin = src.plugin;
-    var skeleton = src.skeleton;
-    var skeletonRenderer = plugin.skeletonRenderer;
+  const plugin = src.plugin;
+  const skeleton = src.skeleton;
+  const skeletonRenderer = plugin.skeletonRenderer;
 
-    var camMatrix = renderer._tempMatrix1;
-    var spriteMatrix = renderer._tempMatrix2;
-    var calcMatrix = renderer._tempMatrix3;
+  const camMatrix = renderer._tempMatrix1;
+  const spriteMatrix = renderer._tempMatrix2;
+  const calcMatrix = renderer._tempMatrix3;
 
-    camera.addToRenderList(src);
+  camera.addToRenderList(src);
 
-    spriteMatrix.applyITRS(src.x, src.y, src.rotation, Math.abs(src.scaleX), Math.abs(src.scaleY));
+  spriteMatrix.applyITRS(src.x, src.y, src.rotation, Math.abs(src.scaleX), Math.abs(src.scaleY));
 
-    camMatrix.copyFrom(camera.matrix);
+  camMatrix.copyFrom(camera.matrix);
 
-    if (parentMatrix)
-    {
-        //  Multiply the camera by the parent matrix
-        camMatrix.multiplyWithOffset(parentMatrix, -camera.scrollX * src.scrollFactorX, -camera.scrollY * src.scrollFactorY);
+  if (parentMatrix) {
+    //  Multiply the camera by the parent matrix
+    camMatrix.multiplyWithOffset(parentMatrix, -camera.scrollX * src.scrollFactorX, -camera.scrollY * src.scrollFactorY);
 
-        //  Undo the camera scroll
-        spriteMatrix.e = src.x;
-        spriteMatrix.f = src.y;
+    //  Undo the camera scroll
+    spriteMatrix.e = src.x;
+    spriteMatrix.f = src.y;
 
-        //  Multiply by the Sprite matrix, store result in calcMatrix
-        camMatrix.multiply(spriteMatrix, calcMatrix);
+    //  Multiply by the Sprite matrix, store result in calcMatrix
+    camMatrix.multiply(spriteMatrix, calcMatrix);
+  } else {
+    spriteMatrix.e -= camera.scrollX * src.scrollFactorX;
+    spriteMatrix.f -= camera.scrollY * src.scrollFactorY;
+
+    //  Multiply by the Sprite matrix, store result in calcMatrix
+    camMatrix.multiply(spriteMatrix, calcMatrix);
+  }
+
+  skeleton.x = calcMatrix.tx;
+  skeleton.y = calcMatrix.ty;
+
+  skeleton.scaleX = calcMatrix.scaleX;
+
+  //  Inverse or we get upside-down skeletons
+  skeleton.scaleY = calcMatrix.scaleY * -1;
+
+  if (src.scaleX < 0) {
+    skeleton.scaleX *= -1;
+
+    src.root.rotation = RadToDeg(calcMatrix.rotationNormalized);
+  } else {
+    //  +90 degrees to account for the difference in Spine vs. Phaser rotation
+    src.root.rotation = Wrap(RadToDeg(CounterClockwise(calcMatrix.rotationNormalized)) + 90, 0, 360);
+  }
+
+  if (src.scaleY < 0) {
+    skeleton.scaleY *= -1;
+
+    if (src.scaleX < 0) {
+      src.root.rotation -= (RadToDeg(calcMatrix.rotationNormalized) * 2);
+    } else {
+      src.root.rotation += (RadToDeg(calcMatrix.rotationNormalized) * 2);
     }
-    else
-    {
-        spriteMatrix.e -= camera.scrollX * src.scrollFactorX;
-        spriteMatrix.f -= camera.scrollY * src.scrollFactorY;
+  }
 
-        //  Multiply by the Sprite matrix, store result in calcMatrix
-        camMatrix.multiply(spriteMatrix, calcMatrix);
-    }
-
-    skeleton.x = calcMatrix.tx;
+  if (camera.renderToTexture) {
     skeleton.y = calcMatrix.ty;
+    skeleton.scaleY *= -1;
+  }
 
-    skeleton.scaleX = calcMatrix.scaleX;
+  skeleton.updateWorldTransform();
 
-    //  Inverse or we get upside-down skeletons
-    skeleton.scaleY = calcMatrix.scaleY * -1;
+  skeletonRenderer.ctx = context;
+  skeletonRenderer.debugRendering = (plugin.drawDebug || src.drawDebug);
 
-    if (src.scaleX < 0)
-    {
-        skeleton.scaleX *= -1;
+  context.save();
 
-        src.root.rotation = RadToDeg(calcMatrix.rotationNormalized);
-    }
-    else
-    {
-        //  +90 degrees to account for the difference in Spine vs. Phaser rotation
-        src.root.rotation = Wrap(RadToDeg(CounterClockwise(calcMatrix.rotationNormalized)) + 90, 0, 360);
-    }
+  skeletonRenderer.draw(skeleton);
 
-    if (src.scaleY < 0)
-    {
-        skeleton.scaleY *= -1;
-
-        if (src.scaleX < 0)
-        {
-            src.root.rotation -= (RadToDeg(calcMatrix.rotationNormalized) * 2);
-        }
-        else
-        {
-            src.root.rotation += (RadToDeg(calcMatrix.rotationNormalized) * 2);
-        }
-    }
-
-    if (camera.renderToTexture)
-    {
-        skeleton.y = calcMatrix.ty;
-        skeleton.scaleY *= -1;
-    }
-
-    skeleton.updateWorldTransform();
-
-    skeletonRenderer.ctx = context;
-    skeletonRenderer.debugRendering = (plugin.drawDebug || src.drawDebug);
-
-    context.save();
-
-    skeletonRenderer.draw(skeleton);
-
-    context.restore();
+  context.restore();
 };
 
 module.exports = SpineGameObjectCanvasRenderer;
