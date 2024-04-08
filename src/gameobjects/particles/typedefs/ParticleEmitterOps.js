dc2cf8f48e35c5144fa1e410ec5dc11c9e3efb81
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:02 -0400

    improve

--- src/gameobjects/particles/typedefs/ParticleEmitterOps.js
+++ src/gameobjects/particles/typedefs/ParticleEmitterOps.js
@@ -1,27 +1,114 @@
 /**
- * @typedef {object} Phaser.Types.GameObjects.Particles.ParticleEmitterOps
- * @since 3.60.0
- *
- * @property {Phaser.GameObjects.Particles.EmitterOp} accelerationX - The accelerationX EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} accelerationY - The accelerationY EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} alpha - The alpha EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} angle - The angle EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} bounce - The bounce EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterColorOp} color - The color EmitterColorOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} delay - The delay EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} hold - The hold EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} lifespan - The lifespan EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} maxVelocityX - The maxVelocityX EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} maxVelocityY - The maxVelocityY EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} moveToX - The moveToX EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} moveToY - The moveToY EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} quantity - The quantity EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} rotate - The rotate EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} scaleX - The scaleX EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} scaleY - The scaleY EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} speedX - The speedX EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} speedY - The speedY EmitterOp instance. This is an onEmit operator only.
- * @property {Phaser.GameObjects.Particles.EmitterOp} tint - The tint EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} x - The x EmitterOp instance. This is an onEmit and onUpdate operator.
- * @property {Phaser.GameObjects.Particles.EmitterOp} y - The y EmitterOp instance. This is an onEmit and onUpdate operator.
+ * Options for configuring a Phaser Particle Emitter.
  */
+export interface PhaserParticleEmitterConfigOptions {
+  /**
+   * The accelerationX operator instance. This is an onEmit and onUpdate operator.
+   */
+  accelerationX: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The accelerationY operator instance. This is an onEmit and onUpdate operator.
+   */
+  accelerationY: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The alpha operator instance. This is an onEmit and onUpdate operator.
+   */
+  alpha: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The angle operator instance. This is an onEmit operator only.
+   */
+  angle: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The bounce operator instance. This is an onEmit and onUpdate operator.
+   */
+  bounce: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The color operator instance. This is an onEmit and onUpdate operator.
+   */
+  color: Phaser.GameObjects.Particles.EmitterColorOp;
+
+  /**
+   * The delay operator instance. This is an onEmit operator only.
+   */
+  delay: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The hold operator instance. This is an onEmit operator only.
+   */
+  hold: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The lifespan operator instance. This is an onEmit operator only.
+   */
+  lifespan: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The maxVelocityX operator instance. This is an onEmit and onUpdate operator.
+   */
+  maxVelocityX: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The maxVelocityY operator instance. This is an onEmit and onUpdate operator.
+   */
+  maxVelocityY: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The moveToX operator instance. This is an onEmit and onUpdate operator.
+   */
+  moveToX: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The moveToY operator instance. This is an onEmit and onUpdate operator.
+   */
+  moveToY: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The quantity operator instance. This is an onEmit operator only.
+   */
+  quantity: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The rotate operator instance. This is an onEmit and onUpdate operator.
+   */
+  rotate: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The scaleX operator instance. This is an onEmit and onUpdate operator.
+   */
+  scaleX: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The scaleY operator instance. This is an onEmit and onUpdate operator.
+   */
+  scaleY: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The speedX operator instance. This is an onEmit operator only.
+   */
+  speedX: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The speedY operator instance. This is an onEmit operator only.
+   */
+  speedY: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The tint operator instance. This is an onEmit and onUpdate operator.
+   */
+  tint: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The x operator instance. This is an onEmit and onUpdate operator.
+   */
+  x: Phaser.GameObjects.Particles.EmitterOp;
+
+  /**
+   * The y operator instance. This is an onEmit and onUpdate operator.
+   */
+  y: Phaser.GameObjects.Particles.EmitterOp;
+}
