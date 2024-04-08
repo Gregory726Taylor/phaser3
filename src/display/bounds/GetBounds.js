224b2d1e51bc57b1897845a8c761dacee3e10b4a
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:01 -0400

    improve

--- src/display/bounds/GetBounds.js
+++ src/display/bounds/GetBounds.js
@@ -1,39 +1,32 @@
 /**
- * @author       samme
- * @copyright    2013-2024 Phaser Studio Inc.
- * @license      {@link https://opensource.org/licenses/MIT|MIT License}
- */
-
-var GetBottom = require('./GetBottom');
-var GetLeft = require('./GetLeft');
-var GetRight = require('./GetRight');
-var GetTop = require('./GetTop');
-var Rectangle = require('../../geom/rectangle/Rectangle');
-
-/**
  * Returns the unrotated bounds of the Game Object as a rectangle.
  *
- * @function Phaser.Display.Bounds.GetBounds
+ * @function getBounds
  * @since 3.24.0
  *
  * @param {Phaser.GameObjects.GameObject} gameObject - The Game Object to get the bounds value from.
- * @param {(Phaser.Geom.Rectangle|object)} [output] - An object to store the values in. If not provided a new Rectangle will be created.
+ * @param {Phaser.Geom.Rectangle|object} [output] - An object to store the values in. If not provided a new Rectangle will be created.
  *
- * @return {(Phaser.Geom.Rectangle|object)} - The bounds of the Game Object.
+ * @return {Phaser.Geom.Rectangle|object} - The bounds of the Game Object.
  */
-var GetBounds = function (gameObject, output)
-{
-    if (output === undefined) { output = new Rectangle(); }
+import { Rectangle } from '../../geom/rectangle/Rectangle';
+import { GetBottom } from './GetBottom';
+import { GetLeft } from './GetLeft';
+import { GetRight } from './GetRight';
+import { GetTop } from './GetTop';
 
-    var left = GetLeft(gameObject);
-    var top = GetTop(gameObject);
+const getBounds = (gameObject: Phaser.GameObjects.GameObject, output?: Phaser.Geom.Rectangle | object): Phaser.Geom.Rectangle | object => {
+  if (output === undefined) { output = new Rectangle(); }
 
-    output.x = left;
-    output.y = top;
-    output.width = GetRight(gameObject) - left;
-    output.height = GetBottom(gameObject) - top;
+  const left = GetLeft(gameObject);
+  const top = GetTop(gameObject);
 
-    return output;
+  output.x = left;
+  output.y = top;
+  output.width = GetRight(gameObject) - left;
+  output.height = GetBottom(gameObject) - top;
+
+  return output;
 };
 
-module.exports = GetBounds;
+export default getBounds;
