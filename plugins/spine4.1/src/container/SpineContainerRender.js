02dcf07c96d40e47ee2f37b155024c900e732c85
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:09 -0400

    improve

--- plugins/spine4.1/src/container/SpineContainerRender.js
+++ plugins/spine4.1/src/container/SpineContainerRender.js
@@ -1,25 +1,3 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
- * @copyright    2022 Photon Storm Ltd.
- * @license      {@link https://opensource.org/licenses/MIT|MIT License}
- */
-
-var renderWebGL = require('../../../../src/utils/NOOP');
-var renderCanvas = require('../../../../src/utils/NOOP');
-
-if (typeof WEBGL_RENDERER)
-{
-    renderWebGL = require('./SpineContainerWebGLRenderer');
-}
-
-if (typeof CANVAS_RENDERER)
-{
-    renderCanvas = require('./SpineContainerCanvasRenderer');
-}
-
-module.exports = {
-
-    renderWebGL: renderWebGL,
-    renderCanvas: renderCanvas
-
-};
+ * @copyright    2
