929d6726f79df2e0dd38de036a64e653de4b5c2d
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:16 -0400

    improve

--- plugins/layer3d/Layer3DCreator.js
+++ plugins/layer3d/Layer3DCreator.js
@@ -1,12 +1,12 @@
 /**
  * @author       Richard Davey <rich@photonstorm.com>
- * @copyright    2020 Photon Storm Ltd.
+ * @copyright    2023 Photon Storm Ltd.
  * @license      {@link https://opensource.org/licenses/MIT|MIT License}
  */
 
-var BuildGameObject = require('../BuildGameObject');
-var GameObjectCreator = require('../GameObjectCreator');
-var Layer3D = require('./Layer3D');
+const { BuildGameObject } = require('../BuildGameObject');
+const { GameObjectCreator } = require('../GameObjectCreator');
+const { Layer3D } = require('./Layer3D');
 
 /**
  * Creates a new Layer3D Game Object and returns it.
@@ -15,26 +15,16 @@ var Layer3D = require('./Layer3D');
  *
  * @method Phaser.GameObjects.GameObjectCreator#layer3d
  * @since 3.50.0
- *
  * @param {Phaser.Types.GameObjects.GameObjectConfig} config - The configuration object this Game Object will use to create itself.
- * @param {boolean} [addToScene] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
- *
+ * @param {boolean} [addToScene=config.add] - Add this Game Object to the Scene after creating it? If set this argument overrides the `add` property in the config object.
  * @return {Phaser.GameObjects.Layer3D} The Game Object that was created.
  */
-GameObjectCreator.register('layer3d', function (config, addToScene)
-{
-    if (config === undefined) { config = {}; }
-
-    var layer = new Layer3D(this.scene, 0, 0);
-
-    if (addToScene !== undefined)
-    {
-        config.add = addToScene;
-    }
-
-    BuildGameObject(this.scene, layer, config);
-
-    return layer;
+GameObjectCreator.register('layer3d', (config, addToScene = config.add) => {
+  config.add = addToScene;
+  const layer = new Layer3D(this.scene, 0, 0, config);
+  BuildGameObject(this.scene, layer, config);
+  return layer;
 });
 
 //  When registering a factory function 'this' refers to the GameObjectCreator context.
+
