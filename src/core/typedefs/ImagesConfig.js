04e7d1c3db73067a86effebf0fd3241f9e18a815
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:07 -0400

    improve

--- src/core/typedefs/ImagesConfig.js
+++ src/core/typedefs/ImagesConfig.js
@@ -2,7 +2,11 @@
  * @typedef {object} Phaser.Types.Core.ImagesConfig
  * @since 3.0.0
  *
+ * This object contains configuration properties for the Images module in Phaser.
+ * It includes options for setting default, missing, and white textures using base64 encoded image files.
+ *
  * @property {string} [default] - A base64 encoded image file to use as the 'default' texture.
  * @property {string} [missing] - A base64 encoded image file to use as the 'missing' texture.
  * @property {string} [white] - A base64 encoded image file to use as the 'white' texture.
  */
+
