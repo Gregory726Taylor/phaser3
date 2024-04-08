cfe0dae0c9a3319fd87c3413cfaef6697fec9d96
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:23:11 -0400

    improve

--- src/gameobjects/particles/typedefs/EmitterOpRandomConfig.js
+++ src/gameobjects/particles/typedefs/EmitterOpRandomConfig.js
@@ -1,8 +1,9 @@
 /**
- * Defines an operation yielding a random value within a range.
+ * Defines an operation yielding a random value within a given range.
  * 
- * @typedef {object} Phaser.Types.GameObjects.Particles.EmitterOpRandomConfig
+ * @typedef {object} Phaser.Types.GameObjects.Particles.EmitterOpRandomRangeConfig
  * @since 3.0.0
  *
- * @property {number[]} random - The minimum and maximum values, as [min, max].
+ * @property {[number, number]} random - The minimum and maximum values of the range.
+ * The resulting random value will be greater than or equal to the minimum and less than the maximum.
  */
