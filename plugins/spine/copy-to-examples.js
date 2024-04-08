3e0b5a3a82b8cb7a2761ae10aee7ab9fe91c4c80
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:12 -0400

    improve

--- plugins/spine/copy-to-examples.js
+++ plugins/spine/copy-to-examples.js
@@ -1,20 +1,20 @@
-var fs = require('fs-extra');
+const fs = require('fs-extra');
 
-var source = './plugins/spine/dist/';
-var dest = '../phaser3-examples/public/plugins/3.8.95/';
+const source = './plugins/spine/dist/';
+const destinations = [
+  { dest: '../phaser3-examples/public/plugins/3.8.95/', name: 'phaser3-examples' },
+  { dest: '../100-phaser3-snippets/public/libs/', name: '100-phaser3-snippets' },
+];
 
-if (fs.existsSync(dest))
-{
-    fs.copySync(source, dest, { overwrite: true });
-}
-else
-{
-    console.log('Copy-to-Examples failed: Phaser 3 Examples not present at ../phaser3-examples');
-}
-
-dest = '../100-phaser3-snippets/public/libs/';
-
-if (fs.existsSync(dest))
-{
-    fs.copySync(source, dest, { overwrite: true });
+for (const dest of destinations) {
+  if (fs.existsSync(dest.dest)) {
+    try {
+      fs.copySync(source, dest.dest, { overwrite: true });
+      console.log(`Copied files to ${dest.name} successfully.`);
+    } catch (err) {
+      console.error(`Error copying files to ${dest.name}:`, err);
+    }
+  } else {
+    console.log(`Copy-to-${dest.name} failed: Phaser 3 Examples not present at ${dest.dest}`);
+  }
 }
