2b500dfdf039bfd83731dd56e409f27d4137a26a
Gregory Taylor <71768337+Gregory726Taylor@users.noreply.github.com>
2024-04-08 17:22:09 -0400

    improve

--- plugins/camera3d/webpack.config.js
+++ plugins/camera3d/webpack.config.js
@@ -1,63 +1,52 @@
 'use strict';
 
+const path = require('path');
 const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const exec = require('child_process').exec;
-const RemovePlugin = require('remove-files-webpack-plugin');
+
+const srcDir = path.resolve(__dirname, 'src');
+const distDir = path.resolve(__dirname, 'dist');
 
 module.exports = {
-    mode: 'production',
-
-    context: `${__dirname}/src/`,
-
-    entry: {
-        camera3d: './Camera3DPlugin.js',
-        'camera3d.min': './Camera3DPlugin.js'
-    },
-
-    output: {
-        path: `${__dirname}/dist/`,
-        filename: '[name].js',
-        library: 'Camera3DPlugin',
-        libraryTarget: 'var'
-    },
-
-    performance: { hints: false },
-
-    optimization: {
-        minimizer: [
-            new UglifyJSPlugin({
-                include: /\.min\.js$/,
-                parallel: true,
-                sourceMap: false,
-                uglifyOptions: {
-                    compress: true,
-                    ie8: false,
-                    ecma: 5,
-                    output: {comments: false},
-                    warnings: false
-                },
-                warningsFilter: () => false
-            })
-        ]
-    },
-
-    plugins: [
-        new RemovePlugin({
-            before: {
-                root: './plugins/camera3d/dist/',
-                include: [ 'camera3d.js', 'camera3d.min.js' ]
-            }
-        }),
-        {
-            apply: (compiler) => {
-                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-                    exec('node plugins/camera3d/copy-to-examples.js', (err, stdout, stderr) => {
-                        if (stdout) process.stdout.write(stdout);
-                        if (stderr) process.stderr.write(stderr);
-                    });
-                });
-            }
-        }
+  mode: 'production',
+  context: srcDir,
+  entry: {
+    camera3d: './Camera3DPlugin.js',
+    'camera3d.min': './Camera3DPlugin.js'
+  },
+  output: {
+    path: distDir,
+    filename: '[name].js',
+    library: 'Camera3DPlugin',
+    libraryTarget: 'var'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        include: /\.min\.js$/,
+        parallel: true,
+        sourceMap: false,
+        uglifyOptions: {
+          compress: true,
+          ie8: false,
+          ecma: 5,
+          output: {comments: false},
+          warnings: false
+        },
+        warningsFilter: () => false
+      })
     ]
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: '# Camera3DPlugin - Do not modify this file',
+      entryOnly: true,
+      exclude: /\.min\.js$/
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 };
+
